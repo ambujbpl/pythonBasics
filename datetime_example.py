173aced9c619b5911d560209e9d6b6156e6eff81
ambujdubey <ambuj@ideata-analytics.com>
2020-01-27 19:28:54 +0530

    date time work in python

--- datetime_example.py
+++ datetime_example.py
@@ -10,7 +10,7 @@ print(repr(dob))# like type of in js
 day_since_birthday = (today - dob).days;
 print(day_since_birthday);#1647 9469 11131
 
-# add some day in today's
+# add some(Exa :- 10 more day) day in current day (today)
 addTimeDelta = datetime.timedelta(days=10);
 print(today + addTimeDelta)
 
@@ -33,6 +33,33 @@ print(datetime.time(9,43,50,10))
 print(todayDT.time())
 
 # timedelta
+# add some(Exa :- 10 more hour) hour in current time (today)
 hour_delta = datetime.timedelta(hours=10);
 print(todayDT + hour_delta)
 
+
+#pip install --user pytz    set Time zone in python
+import pytz;
+datetime_today_utc = datetime.datetime.now(tz=pytz.UTC);
+print(datetime_today_utc);
+
+datetime_today_pacific = datetime_today_utc.astimezone(pytz.timezone('US/Pacific'));
+print(datetime_today_pacific);
+
+#print all time zone in python
+for tz in pytz.all_timezones:
+    print(tz)
+
+
+
+#string formatting with dates in python
+#1 January 27, 2020
+print(datetime_today_pacific.strftime('%B %d, %Y'));
+
+#2 Take input as 'January 27, 2020' and output as '2020-01-27'
+datetime_today_str = datetime.datetime.strptime('January 27, 2020','%B %d, %Y')
+print(datetime_today_str);
+print(repr(datetime_today_str));
+
+
+# pip install maya
